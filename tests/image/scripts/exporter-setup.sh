c2bf2c860efcbb1e7b08d1bbb5f6598af5f1cba3
MarioLopezz <mlopezzbueno@gmail.com>
2024-04-08 18:12:34 +0200

    changes in solr image

--- tests/image/scripts/exporter-setup.sh
+++ tests/image/scripts/exporter-setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-/opt/solr-8.11.1/contrib/prometheus-exporter/bin/solr-exporter -p 9983 -z zk-0.zk-hs.default.svc.cluster.local:2181,zk-1.zk-hs.default.svc.cluster.local:2181,zk-2.zk-hs.default.svc.cluster.local:2181 -f /opt/solr-8.11.1/contrib/prometheus-exporter/conf/solr-exporter-config.xml &
+max_try=${MAX_TRY:-12}
+wait_seconds=${WAIT_SECONDS:-10}
+if ! /opt/docker-solr/scripts/wait-for-solr.sh --max-attempts "$max_try" --wait-seconds "$wait_seconds"; then
+    exit 1
+fi
+
+/opt/solr-$SOLR_VERSION/contrib/prometheus-exporter/bin/solr-exporter -p 9983 -z $ZK_HOST -f /opt/solr-$SOLR_VERSION/contrib/prometheus-exporter/conf/solr-exporter-config.xml
